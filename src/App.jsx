367fb7fef106ec34fbfa1e1d511e5b58cf2e2844
JackJack2405 <chutikankin7442@gmail.com>
2025-12-02 12:30:43 +0700

    update question

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import Castle from "./components/01_Castle"
+import { useState } from "react";
+import Castle from "./components/01_Castle";
+
+// state variable vs standard JS variable (var,let,const)
+// In React we can use useState (a react Hook or built-in function/method) to create a state variable
 
 export default function App() {
+
+
+  const [question, setQuestion ] = useState("");
+
+
+  const handleQuestion = (e) => {
+setQuestion(e.target.value);
+  }
+
   return (
-    <div>
+    <div className="pb-80 py-10 gap-y-4 flex flex-col justify-center items-center min-h-screen bg-gray-800 text-white">
       <h1> Learn React with Castle Rooms🏰 </h1>
       <p>Message for the Secret Room🐴 </p>
-      <textarea />
-      <p>⌛Waiting for a message...</p>
-      <p>Reply from the Secret Room: </p>
-      <p>⌛Waiting for a reply...</p>
+      <p className="text-purple-">Message for the Secret Room🐎🎠✉️ : </p>
+      <textarea 
+        value={question} 
+        onChange={handleQuestion} 
+        className="bg-white text-black px-2 py-1"/>
+      <p className="text-yellow-300">
+        {question ? `✅${question}` : "⌛Waiting for a message..." }
+      </p>
+      <p className="text-green-300">Reply from the Secret Room: </p>
+      <p className="text-yellow-300">⌛Waiting for a reply...</p>
       <Castle />
     </div>
   );
--- src/components/08_Nook.jsx
+++ src/components/08_Nook.jsx
@@ -2,7 +2,7 @@ import SecretRoom from "./09_SecretRoom";
 
 export default function Nook(){
     return(
-        <div className="flex flex-col justify-center items-center pt-10 bg-violet-500 w-[90%]">
+        <div className="flex flex-col justify-center items-center pt-10 bg-fuchsia-400 w-[90%]">
             <h1>Nook</h1>
             <SecretRoom />
         </div>
