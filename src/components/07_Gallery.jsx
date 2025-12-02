3815a6c60747b1e3e25146c7b172b8b66f9e8641
JackJack2405 <chutikankin7442@gmail.com>
2025-12-02 15:21:42 +0700

    update reply box

--- src/components/07_Gallery.jsx
+++ src/components/07_Gallery.jsx
@@ -1,10 +1,10 @@
 import Nook from "./08_Nook";
 
-export default function Gallery(){
+export default function Gallery({question, answer,handleAnswer}){
     return(
         <div className="flex flex-col justify-center items-center pt-10 bg-rose-500 w-[90%]">
             <h1>Gallery</h1>
-            <Nook />
+            <Nook question={question} answer={answer} handleAnswer={handleAnswer}/>
         </div>
     );
 }
--- src/components/09_SecretRoom.jsx
+++ src/components/09_SecretRoom.jsx
@@ -1,9 +1,22 @@
 //import SecretRoom from "./09_SecretRoom";
 
-export default function SecretRoom(){
+export default function SecretRoom({question, answer,handleAnswer }){
     return(
         <div className="flex flex-col justify-center items-center pt-10 bg-pink-500 w-[90%]">
             <h1>SecretRoom</h1>
+            <p className="text-purple-500">Message from the outside: </p>
+            <p className="text-yellow-300">
+                {question ? `✅${question}` : "⌛Waiting for a message..." }
+            </p>
+            <p>Reply to the outside: </p>
+            <textarea 
+            value = {answer}
+            onChange={handleAnswer}
+            className="w-[150px] bg-white text-black rounded px-2 py-1" 
+            />
+            <p className="text-yellow-300">{answer ? `✅${answer}`: "⌛Waiting for a reply" }</p>
+            
+        
             {/* <p>🗝️ This is the final room.</p> */}
         </div>
     );
